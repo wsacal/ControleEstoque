2aa24fd0a4519adb8ab4741e4eaffad11d0a9b51
weslei sacal <wsacal@gmail.com>
2018-12-05 22:30:10 -0300

    atualizando.

--- ControleEstoque.Web/Controllers/ContaController.cs
+++ ControleEstoque.Web/Controllers/ContaController.cs
@@ -47,5 +47,13 @@ namespace ControleEstoque.Web.Controllers
 
             return View(login);
         }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public ActionResult LogOff()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Index", "Home");
+        }
     }
 }
