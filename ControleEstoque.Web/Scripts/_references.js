19975268d29c28459ee41bc088f80f89f1557dcc
weslei sacal <wsacal@gmail.com>
2018-12-24 13:57:04 -0300

    alterando

---
+++ ControleEstoque.Web/Scripts/_references.js
@@ -0,0 +1,9 @@
+﻿/// <autosync enabled = "true" />
+/// <reference path = "ai.0.22.19-build00125.js" /> 
+/// <reference path = "bootbox.min.js" />
+/// <reference path = "bootstrap.js" />
+/// <reference path = "jquery.validate.js" />
+/// <reference path = "jquery.validate.unobtrusive.js" />
+/// <reference path = "jquery-3.3.1.js" />
+/// <reference path = "modernizr-2.8.3.js" />
+/// <reference path = "respond.js" />
